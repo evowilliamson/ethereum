942d922ae40bbaa97e5e98c80213465e5669654f
ivo_willemsen@hotmail.com <hyperhyper1A2a>
2018-06-20 13:30:48 +0200

    changes after review 2

--- splitter/migrations/2_deploy_contracts.js
+++ splitter/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Splitter = artifacts.require("./Splitter.sol");
+var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Splitter);
+  deployer.deploy(SimpleStorage);
 };
