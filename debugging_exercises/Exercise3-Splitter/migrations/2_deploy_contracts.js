4d4f127494fdf47e7e1a01b7073b7ef931c1a2c7
evowilliamson <ivo_willemsen@hotmail.com>
2019-11-12 15:11:38 +0100

    Reorg

--- debugging_exercises/Exercise3-Splitter/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const HackerOne = artifacts.require("./HackerOne.sol");
-const HackerTwo = artifacts.require("./HackerTwo.sol");
-const Splitter = artifacts.require("./Splitter.sol");
-
-module.exports = (deployer) => {}
-        
-    
--- debugging_exercises/Exercise3-Splitter/utils/randomIntIn.js
+++
@@ -1,3 +0,0 @@
-module.exports = function randomIntIn(from, to) {
-	return Math.floor(Math.random() * Math.floor(to)) + Math.floor(from);
-}
